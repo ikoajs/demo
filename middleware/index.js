3ed06908982e332f46afb3d7f842f581bf057e78
Jiangtao <321jiangtao@gmail.com>
2017-12-17 10:14:31 +0800

    fix: 整理代码

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,7 @@
-// const Koa = require('koa')
-const Koa = require('../../ikoa/lib/application')
+const Koa = require('koa')
+const koaStatic = require('ikoa-static')
 const app = new Koa()
 const port = process.argv.slice(2).pop() || 2000
-const koaStatic = require('./static')
 
 app.use(koaStatic({ dir: '/Users/jerret/Places/Resources' }))
 app.listen(port, () => {
